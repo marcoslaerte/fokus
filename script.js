cc41f282e842415bdc3c623b51cbf7a4b0a3172d
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-12-18 19:49:03 -0300

    Convertendo e mostrando o tempo de acordo com o contexto

--- script.js
+++ script.js
@@ -28,21 +28,25 @@ musicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco');
     focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+    mostrarTempo();
     botoes.forEach(function(botao) {
         botao.classList.remove('active');
     });
@@ -71,7 +75,7 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0) {
-        //audioTempoFinalizado.play();
+        audioTempoFinalizado.play();
         alert('Tempo finalizado!');
         zerar();
         return;
@@ -102,8 +106,9 @@ function zerar() {
 }
 
 function mostrarTempo() {
-    const tempo = tempoDecorridoEmSegundos;
-    tempoNaTela.innerHTML = `${tempo}`;
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    tempoFormatado = tempo.toLocaleTimeString('pt-BR', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = tempoFormatado;
 }
 
 mostrarTempo();
